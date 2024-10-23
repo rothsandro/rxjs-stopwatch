60d7d56d119dd2c6e196d8518c83335aa31fff17
Sandro Roth <16229645+rothsandro@users.noreply.github.com>
2024-10-23 18:14:06 +0200

    Add solution

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,33 @@
 import "./style.css";
 import { letsGetThePartyStarted } from "./party";
-import { of } from "rxjs";
+import {
+  endWith,
+  fromEvent,
+  interval,
+  map,
+  repeat,
+  switchMap,
+  takeUntil,
+  tap,
+} from "rxjs";
 
 const startBtn = document.getElementById("start")!;
 const stopBtn = document.getElementById("stop")!;
 const resetBtn = document.getElementById("reset")!;
 const output = document.getElementById("output")!;
 
-const stopwatch$ = of(0);
+const start$ = fromEvent(startBtn, "click");
+const stop$ = fromEvent(stopBtn, "click");
+const reset$ = fromEvent(resetBtn, "click");
 
-stopwatch$.subscribe(console.log);
+const stopwatch$ = start$.pipe(
+  tap(() => letsGetThePartyStarted()),
+  switchMap(() => interval(1000).pipe(takeUntil(stop$))),
+  map((_, idx) => idx + 1),
+  takeUntil(reset$),
+  endWith(0),
+  tap((val) => (output.textContent = `${val}s`)),
+  repeat()
+);
+
+stopwatch$.subscribe();
